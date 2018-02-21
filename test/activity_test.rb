26a10be1564cd158cae5b12fc7f6072a75283141
gijolopez <glopez22@me.com>
2018-02-21 09:17:46 -0700

    first committ and added name method/test

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,11 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_has_name
+    activity = Activity.new('hiking')
+
+    assert_instance_of Activity, activity
+    assert_equal 'hiking', activity.name 
+  end
 end
