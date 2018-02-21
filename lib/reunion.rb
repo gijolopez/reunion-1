f272eedde6055fd2ec858c6a5d89a19b2528ef09
gijolopez <glopez22@me.com>
2018-02-21 10:58:31 -0700

    finishing up iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,9 +1,11 @@
 require './lib/activity'
 
 class Reunion
-  attr_reader :location
+  attr_reader :location,
+              :activities
 
-  def initialize(location)
+  def initialize(location, activities = [])
     @location = location
+    @activities = activities
   end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -10,4 +10,22 @@ class ReunionTest < Minitest::Test
     assert_instance_of Reunion, reunion
     assert_equal "Aspen", reunion.location
   end
+
+  def test_reunion_has_activities
+    hiking  = Activity.new('hiking')
+    archery = Activity.new('archery')
+    reunion = Reunion.new('Aspen', [hiking, archery])
+
+    assert_instance_of Array, reunion.activities
+    assert_instance_of Activity, reunion.activities[0]
+  end
+
+  def test_activities_can_be_added_to_reunion
+    hiking  = Activity.new('hiking')
+    archery = Activity.new('archery')
+    reunion = Reunion.new('Aspen', [hiking, archery])
+
+    reunion.add_activity('biking')
+
+  end
 end
